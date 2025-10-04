1d265f2c2cd423ffa251d6266896e29ba6074ee7
Anshdeep Bansal <udaibansal123@gmail.com>
2025-10-05 00:39:34 +0530

    map in go

--- first/main.go
+++ first/main.go
@@ -10,4 +10,9 @@ func main() {
 	var arr [2]int
 	arr[0] = 2
 	fmt.Println(arr)
+	//create a map in golang and add some values to it te=hen print it
+	map1 := make(map[string]int)
+	map1["anshdeep"] = 23
+	map1["john"] = 30
+	fmt.Println(map1)
 }
